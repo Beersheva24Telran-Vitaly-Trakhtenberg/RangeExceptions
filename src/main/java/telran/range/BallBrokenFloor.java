93fc1b5390a6af5e6aa7c1fc09961a171a4b14ec
Vitaly Trakhtenberg <webdev@tca.by>
2024-08-07 15:08:51 +0300

    Initial setup of the project. Did clone of Yuri's repository. Ready to own developing process.

---
+++ src/main/java/telran/range/BallBrokenFloor.java
@@ -0,0 +1,31 @@
+package telran.range;
+
+import java.util.Random;
+
+public class BallBrokenFloor
+{
+    private int nFloors;
+    private int minBrokenFloor;
+
+    public BallBrokenFloor(int nFloors)
+    {
+        this.nFloors = nFloors;
+        minBrokenFloor = new Random().nextInt(1, nFloors + 1);
+    }
+
+    public void checkFloor (int floor) throws Exception
+    {
+        if (floor > nFloors) {
+            throw new IllegalArgumentException();
+        }
+        if (floor >= minBrokenFloor) {
+            throw new Exception("ball is broken");
+        }
+    }
+
+    public int getMinBrokenFloor()
+    {
+        //uses only for testing purposes
+        return minBrokenFloor;
+    }
+}
---
+++ src/main/java/telran/range/exceptions/OutOfRangeMaxValueException.java
@@ -0,0 +1,8 @@
+package telran.range.exceptions;
+
+public class OutOfRangeMaxValueException  extends Exception
+{
+    public OutOfRangeMaxValueException(int max, int value) {
+        super(String.format("Out of Range max: %d, number: %d", max, value));
+    }
+}
