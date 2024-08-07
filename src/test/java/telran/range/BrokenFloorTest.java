e06b7ff749317e8a47f5d43dd50cae1943462959
Vitaly Trakhtenberg <webdev@tca.by>
2024-08-07 16:29:44 +0300

    Developed method getMinimalBrokenFloor() using BinarySearch algorithm

--- src/test/java/telran/range/BrokenFloorTest.java
+++ src/test/java/telran/range/BrokenFloorTest.java
@@ -7,15 +7,27 @@ import org.junit.jupiter.api.Test;
 public class BrokenFloorTest
 {
 
-    private int getMinimalBrokenFloor(BallBrokenFloor bbf) {
-        //TODO
-//Using only method checkFloor to find out minimal broken floor by applying
-//binary search algorithm
-        return -1;
+    private int getMinimalBrokenFloor(BallBrokenFloor bbf)
+    {
+        int low_floor = 1;
+        int high_floor = bbf.getNFloors();
+        int result = -1;
+        while (low_floor <= high_floor) {
+            int middle = (low_floor + high_floor) / 2;
+            try {
+                bbf.checkFloor(middle);
+                low_floor = middle + 1;
+            } catch (Exception e) {
+                result = middle;
+                high_floor = middle - 1;
+            }
+        }
+        return result;
     }
 
     @Test
-    void minimalBrokenFloorTest() {
+    void minimalBrokenFloorTest()
+    {
         int [] floors = {200, 17, 1001, 2000};
         for(int i = 0; i < floors.length; i++) {
             BallBrokenFloor bbf = new BallBrokenFloor(floors[i]);
